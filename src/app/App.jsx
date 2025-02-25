6ad4dd2e315585a4c5786d5e4d14aadbbb5e9476
vishalicious213 <vishalicious213@gmail.com>
2025-02-25 12:15:20 -0800

    added renderTool() to App to control which component is rendered from sidebar

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Sidebar from './Sidebar'
 import Connections from './connections/Connections'
 import CoffeeChats from './connections/CoffeeChats'
@@ -5,19 +6,27 @@ import Requests from './connections/Requests'
 import './App.css'
 
 function App() {
+  const [toolToRender, setToolToRender] = useState('connections')
+
+  function renderTool() {
+    switch (toolToRender) {
+      case 'connections': return <Connections />
+      case 'coffeechats': return <CoffeeChats />
+      case 'requests': return <Requests />
+    }
+  }
+
   return (
-    <>
+    <div className='app-container'>
       <section>
         <div>App</div>
         <Sidebar />
       </section>
 
       <main>
-          <Connections />
-          <CoffeeChats />
-          <Requests />
+        {renderTool()}
       </main>
-    </>
+    </div>
   )
 }
 
