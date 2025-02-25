87c4a503bf14314f48920cb405fa76f6ba48ea8d
vishalicious213 <vishalicious213@gmail.com>
2025-02-25 11:34:23 -0800

    drafted Connections, CoffeeChats & Requests. rendered in Dashboard

--- src/app/Dashboard.jsx
+++ src/app/Dashboard.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import Connections from './connections/Connections'
+import CoffeeChats from './connections/CoffeeChats'
+import Requests from './connections/Requests'
 
 const Dashboard = () => {
     return (
         <main>
             <Sidebar />
+            <Connections />
+            <CoffeeChats />
+            <Requests />
         </main>
     )
 }
