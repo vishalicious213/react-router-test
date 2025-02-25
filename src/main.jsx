88b651add9f07b1170f153063ab8ec9eeb13b926
vishalicious213 <vishalicious213@gmail.com>
2025-02-24 18:01:27 -0800

    added nested routes for about-us, faw, contact to layout in main

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
-import App from './App.jsx'
-import Home from './Home.jsx'
 import AboutUs from './AboutUs.jsx'
+import App from './App.jsx'
 import Contact from './Contact.jsx'
 import Faq from './Faq.jsx'
+import Home from './Home.jsx'
+import Layout from './Layout.jsx'
 
 const isLoggedIn = false
 
@@ -18,11 +19,11 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeToRender />} />
-        <Route path="about-us" element={<AboutUs />} />
-        <Route path="faq" element={<Faq />} />
-        <Route path="contact" element={<Contact />} />
-        {/* <Route path="app" element={<App />} /> */}
+        <Route path="/" element={<Layout />}>
+          <Route path="about-us" element={<AboutUs />} />
+          <Route path="faq" element={<Faq />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
