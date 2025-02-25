7c4a6b6aba72eb7fb44415119565127d7ab65e37
vishalicious213 <vishalicious213@gmail.com>
2025-02-25 12:39:22 -0800

    Sidebar: added My Profile section & menu items. Added basic styling

--- src/app/Sidebar.jsx
+++ src/app/Sidebar.jsx
@@ -7,9 +7,15 @@ const Sidebar = ({ setToolToRender }) => {
 
     return (
         <section className="sidebar">
+            <h2>My Dashboard</h2>
             <div onClick={() => handleClick("connections")}>Connections</div>
             <div onClick={() => handleClick("coffeechats")}>Coffee Chats</div>
             <div onClick={() => handleClick("requests")}>Requests</div>
+
+            <h2>My Profile</h2>
+            <div>Personal Details</div>
+            <div>Availability</div>
+            <div>Social Links</div>
         </section>
     )
 }
