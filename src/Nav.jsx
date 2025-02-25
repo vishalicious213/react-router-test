3b987a125f4c0dd9d64ead2171b5544dc5425a9b
vishalicious213 <vishalicious213@gmail.com>
2025-02-24 18:18:52 -0800

    rendering Login in Nav

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Link } from "react-router"
+import Login from './Login'
 
 const Nav = () => {
     return (
         <>
             <nav>
-                <Link className='nav-link' to="/">Home</Link>
-                <Link className='nav-link' to="/about-us">About Us</Link>
-                <Link className='nav-link' to="/faq">FAQ</Link>
-                <Link className='nav-link' to="/contact">Contact</Link>
-                {/* <Link className='nav-link' to="/app">App</Link> */}
+                <section>
+                    <Link className='nav-link' to="/">Home</Link>
+                    <Link className='nav-link' to="/about-us">About Us</Link>
+                    <Link className='nav-link' to="/faq">FAQ</Link>
+                    <Link className='nav-link' to="/contact">Contact</Link>
+                </section>
+                <Login />
             </nav>
         </>
     )
