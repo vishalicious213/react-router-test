80117232f4e4776b26cc93be1812dc69b8500d38
vishalicious213 <vishalicious213@gmail.com>
2025-02-25 13:22:42 -0800

    updated Login with a Sign Up button. it only shows when user isn't logged in

--- src/layout/Login.jsx
+++ src/layout/Login.jsx
@@ -3,14 +3,19 @@ import React, { useState } from 'react'
 const Login = ({showApp}) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-    function handleClick() {
+    function handleLogin() {
         setIsLoggedIn(prevState => !prevState)
         showApp()
     }
 
+    function handleSignup() {
+        console.log("Sign Up clicked")
+    }
+
     return (
         <section>
-            <div className="login-btn" onClick={handleClick}>{isLoggedIn ? "Logout" : "Login"}</div>
+            {isLoggedIn ? "" : <div className="login-btn" onClick={handleSignup}>Sign Up</div>}
+            <div className="login-btn" onClick={handleLogin}>{isLoggedIn ? "Logout" : "Login"}</div>
         </section>
     )
 }
