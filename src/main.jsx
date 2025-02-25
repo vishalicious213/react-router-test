f58d3289ee5c14258dec798ca45b9aeea740c75e
vishalicious213 <vishalicious213@gmail.com>
2025-02-24 18:33:33 -0800

    turned main.jsx into a React function that renders at the root. added useState & placed isLoggedIn into it

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
@@ -9,23 +9,27 @@ import Faq from './Faq.jsx'
 import Home from './Home.jsx'
 import Layout from './Layout.jsx'
 
-const isLoggedIn = false
+function Main() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-const HomeToRender = () => {
-  return isLoggedIn ? <App /> : <Home />
+  const HomeToRender = () => {
+    return isLoggedIn ? <App /> : <Home />
+  }
+
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomeToRender />} />
+            <Route path="about-us" element={<AboutUs />} />
+            <Route path="faq" element={<Faq />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  )
 }
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomeToRender />} />
-          <Route path="about-us" element={<AboutUs />} />
-          <Route path="faq" element={<Faq />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<Main />)
